5d13d77ab3cee4d034d0fda0a66fb5da7be5f52a
catilus <helene.m.piet@gmail.com>
2026-01-25 12:54:53 -0700

    Added unit validation for extracted temperatures

--- rx_poc.sh
+++ rx_poc.sh
@@ -2,6 +2,15 @@
 
 ## Script is run every day at 12PM local time
 
+## ----- FUNCTIONS -----
+
+# Validation of extracted temperatures
+is_temperature() {
+    [[ $1 =~ ^[+-]?[0-9]+$ ]]
+}
+
+## ----- SCRIPT -----
+
 # City and country variable declaration
 city="Casablanca"
 country="Morocco"
@@ -18,14 +27,28 @@ temp_unit=$(grep -oE '°C|°F' weather_report | sed -n '1p')
 # Next day forecasted is at line 13 - second value for Noon
 # Two day forecasted is at line 23 - ditto
 # Three day forecasted is at line 33 - ditto
+declare -a temp_array
 temp_array[0]=$(sed -n '4p' weather_report | grep -oE '[+-]?[0-9]+\([0-9]+\)|[+-]?[0-9]+' | grep -oE '^[+-]?[0-9]+')
+
+if ! is_temperature "${temp_array[0]}"; then
+    echo "ERROR: Invalid temperature data extracted at line4." >> error.log
+    exit 1
+fi
+
+
 line=13
 
+# Extract temperature and validate extracted data before continuing
 for (( i=1; i<=3; i++ )); do
     temp_array[i]=$(sed -n "${line}p" weather_report | 
     grep -oE '[+-]?[0-9]+\([0-9]+\)|[+-]?[0-9]+' | 
     sed -n '2p' | 
     grep -oE '^[+-]?[0-9]+')
+        
+    if ! is_temperature "${temp_array[i]}"; then
+        echo "ERROR: Invalid temperature data extracted at line$line." >> error.log
+        exit 1
+    fi
     
     (( line+=10 ))
 done
@@ -36,19 +59,14 @@ echo "Next-day forecasted temperature (12PM): ${temp_array[1]}$temp_unit"
 echo "2-day forecasted temperature (12PM): ${temp_array[2]}$temp_unit"
 echo "3-day forecasted temperature (12PM): ${temp_array[3]}$temp_unit"
 
-
-# 2. Extract forecasted T for next day at noon into a variable (at line 13)
-# Selects line #13 in file --> extracts only the temperature based on format --> extracts for noon (2nd match) --> extracts only the actual temperature --> adds a + sign when temperature does not have one  
-# sed -n '13p' weather_report | grep -oE '[+-]?[0-9]+\([0-9]+\)|[+-]?[0-9]+' | sed -n '3p' | grep -oE '^[+-]?[0-9]+' | sed -nE 's/(^[0-9]+)/+\1/p'
-# fc_temp=$(sed -n '13p' weather_report | grep -oE '[+-]?[0-9]+\([0-9]+\)|[+-]?[0-9]+' | sed -n '3p' | grep -oE '^[+-]?[0-9]+')
-# echo "Forecasted temperature: $fc_temp$temp_unit"
-
-# 3. Extract current Year, Month and Day into variables
+## Add year, month, day, time, and temperatures to report
 year=$(TZ='$country/$city' date +%Y)
 month=$(TZ='$country/$city' date +%m)
 day=$(TZ='$country/$city' date +%e)
 #echo "$day/$month/$year"
 
 # 4. Merge extracted variables into the report
-echo -e "$year\t$month\t$day\t$obs_temp\t$fc_temp" >> rx_poc.log
+echo -e "$year\t$month\t$day\t${temp_array[0]}\t${temp_array[1]}\t${temp_array[2]}\t${temp_array[3]}" >> rx_poc.log
+
+
 
